2fa445f4639507dbcb607a10ae065ef2811a163b
peter-wairimu <peter.wairimu@student.moringaschool.com>
2022-05-16 13:25:18 +0300

    initial commit

---
+++ main.h
@@ -0,0 +1,24 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <stdbool.h>
+
+// environment variables
+extern char **environ;
+extern __sighandler_t signal(int __sig, __sighandler_t __handler);
+
+
+/* handle built ins */
+int checker(char **cmd, char *buf);
+void prompt_user(void);
+
+#endif
